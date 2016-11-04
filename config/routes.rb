4c356329524f890641a8caff5b5a9c5bb63d179f
Dustin Kahn <dustin@dustinkahn.com>
2016-11-04 14:47:06 -0400

    user model and routes added for omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "auth/:provider/callback", to: "sessions#create"
+  get "signin", to: "sessions#new", as: "signin"
+
+  root "sessions#new"
+
+  resources :auth, only: :show
+  resources :sessions, only: [:destroy, :new]
+  resources :users
+
 end
