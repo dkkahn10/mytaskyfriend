8baecf3b3a7c2d5049b9063c45ac0b5ceeb967e2
Dustin Kahn <dustin@dustinkahn.com>
2016-11-04 16:11:39 -0400

    small change to factory

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -3,7 +3,7 @@ require 'factory_girl'
 FactoryGirl.define do
   factory :user do
     sequence(:email) { |n| "foob@example#{n}.com"}
-    sequence(:name) { |n| "foobington#{n}" }
+    sequence(:username) { |n| "foobington#{n}" }
     sequence(:oauth_uid) { |n| n }
   end
 end
