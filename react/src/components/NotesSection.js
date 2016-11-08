5b5f4bedbb0591e8bbffad0212e721383abecf49
Jun Oh <jsohpennstater@gmail.com>
2016-11-08 16:54:43 -0500

    intergrated API call to save project name in backend

--- react/src/components/NotesSection.js
+++ react/src/components/NotesSection.js
@@ -1,52 +1,52 @@
-import React, {Component} from 'react';
-import Note from './Note';
+// import React, {Component} from 'react';
+// import Note from './Note';
+//
+//
+// class NotesSection extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       selectedProjectId: null
+//     }
+//
+//   this.handleProjectClick = this.handleProjectClick.bind(this);
+//   this.handleNewNoteClick = this.handleNewNoteClick.bind(this);
+//   this.handleEditNoteClick = this.handleEditNoteClick.bind(this);
+//   this.handleDeleteNoteClick = this.handleDeleteNoteClick.bind(this);
+//   };
 
+  // componentDidMount() {
+  //   if (this.props.projectId !== "") {
+    //   .done(data => {
+    //     this.setState({ notes:data });
+    //   });
+    // }
+  // }
 
-class NotesSection extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectedProjectId: null
-    }
-
-  this.handleProjectClick = this.handleProjectClick.bind(this);
-  this.handleNewNoteClick = this.handleNewNoteClick.bind(this);
-  this.handleEditNoteClick = this.handleEditNoteClick.bind(this);
-  this.handleDeleteNoteClick = this.handleDeleteNoteClick.bind(this);
-  };
-
-  componentDidMount() {
-    if this.props.projectId !== "" {
-      .done(data => {
-        this.setState({ notes:data });
-      });
-    }
-  }
-
-
-    render() {
-      let notes = this.props.data.map(note => {
-          let newNoteClick = () =>
-          this.handleNewNoteClick();
-          let editNoteClick = () =>
-          this.handleEditNoteClick();
-          let deleteNoteClick = () =>
-          this.handleDeleteNoteClick();
-        return (
-          <Note
-            newNoteClick={newNoteClick}
-            editNoteClick={editNoteClick}
-            deleteNoteClick={deleteNoteClick}
-          />
-        )
-      });
-
-      return(
-        <div className="projects">
-          {notes}
-        </div>
-      );
-    }
-}
-
-export default NotesSection;
+//
+//     render() {
+//       let notes = this.props.data.map(note => {
+//           let newNoteClick = () =>
+//           this.handleNewNoteClick();
+//           let editNoteClick = () =>
+//           this.handleEditNoteClick();
+//           let deleteNoteClick = () =>
+//           this.handleDeleteNoteClick();
+//         return (
+//           <Note
+//             newNoteClick={newNoteClick}
+//             editNoteClick={editNoteClick}
+//             deleteNoteClick={deleteNoteClick}
+//           />
+//         )
+//       });
+//
+//       return(
+//         <div className="projects">
+//           {notes}
+//         </div>
+//       );
+//     }
+// }
+//
+// export default NotesSection;
