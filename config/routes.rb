09aac66dbd9612f4e024bd9cc979a54ebef0e374
Jun Oh <jsohpennstater@gmail.com>
2016-11-09 15:29:07 -0500

    NotesSection.js to be stateful

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :projects, only: [:index, :create, :destroy]
-      resources :tasks, only: [:index, :create, :destroy]
+      resources :tasks, only: [:index, :create, :destroy, :show]
     end
   end
 
