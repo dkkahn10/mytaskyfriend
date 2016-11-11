b47206ea84fae0f21ec8b585e834a6f699bbe633
jeffreyfriedman <jeff@eurodollaryen.com>
2016-11-11 14:50:32 -0500

    WIP websockets chatrooms

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,6 @@
 class MessagesController < ApplicationController
 
   def create
-    # curl -X POST -H "Content-Type: application/json" -d '{"message": {"content": "test", "chatroom_id": 1} }' http://localhost:3000/messages
-    binding.pry
     message = Message.new(message_params)
     message.user = current_user
     if message.save
