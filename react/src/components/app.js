af6c494e89772b3ed31eb70e93707a591f7b4254
Dustin Kahn <dustin@dustinkahn.com>
2016-11-12 13:03:11 -0500

    missing semi colon

--- react/src/components/app.js
+++ react/src/components/app.js
@@ -13,7 +13,7 @@ class App extends Component {
       editId: "",
     };
     this.handleProjectClick = this.handleProjectClick.bind(this);
-    this.handleFieldChange = this.handleFieldChange.bind(this)
+    this.handleFieldChange = this.handleFieldChange.bind(this);
     this.handleNewProject = this.handleNewProject.bind(this);
     this.handleDeleteProject = this.handleDeleteProject.bind(this);
     this.handleEditProject = this.handleEditProject.bind(this);
