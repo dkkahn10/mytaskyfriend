b3845dd2ea77c249ab540ee0393bb57feefac2df
anniehck <anniehckim@gmail.com>
2016-11-17 16:31:17 -0500

    New Project form changed

--- react/src/components/ProjectsSection.js
+++ react/src/components/ProjectsSection.js
@@ -34,10 +34,6 @@ const ProjectsSection = props => {
         <div className="collapsible-header"><i className="material-icons">place</i>Second</div>
         <div className="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
       </li>
-      <li>
-        <div className="collapsible-header"><i className="material-icons">whatshot</i>Third</div>
-        <div className="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
-      </li>
     </ul>
     </div>
     </div>
