1e6a7cdc2e1d893778a494c41ca07b5c491a64a7
anniehck <anniehckim@gmail.com>
2016-11-18 23:09:21 -0500

    Edit, Cancel buttons reformat

--- react/src/components/ProjectEdit.js
+++ react/src/components/ProjectEdit.js
@@ -7,8 +7,10 @@ const ProjectEdit = props => {
     <div>
       {props.color}
       <input type="text" value={props.editProject} name="editProject" onChange={props.handleFieldChange} />
+      <div className="buttons project-edit">
       <button className="EditProject btn" onClick={props.handleEdit}>Save Edit</button>
       <button className="Cancel btn" onClick={props.handleCancel}>Cancel</button>
+      </div>
     </div>
   )
 }
