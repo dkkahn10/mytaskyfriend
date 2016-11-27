26db3cbccadf1dd2cd62c90b6d5fc85a9b4992d0
jeffreyfriedman <jeff@eurodollaryen.com>
2016-11-27 13:41:30 -0500

    cleanup of binding.pry

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,28 +1,4 @@
 class UsersController < ApplicationController
-#   # may no longer be necessary
-#   def create
-#     @user = User.new(user_params)
-#     @user.email = session[:auth]["info"]["email"]
-#     @user.oauth_uid = session[:auth]["uid"]
-#     @user.facebook_photo = session[:auth]["info"]["image"]
-#
-#     if @user.save
-#       session[:auth].clear
-#       session[:user_id] = @user.id
-#       @user.touch :last_signed_in_at
-#       @user.increment! :sign_in_count
-#       flash[:success] = "Registered successfully."
-#       redirect_to projects_path
-#     else
-#       flash[:alert] = "There was a problem registering."
-#       render :new
-#     end
-#   end
-#
-#   def new
-#     @user = User.new(oauth_uid: session[:auth]["uid"])
-#   end
-#
   def show
     @user = User.find(params[:id])
     if @user.current_photo.nil?
