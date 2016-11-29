add4dd95d02be56a20f981405dd1acf93dae90c7
jeffreyfriedman <jeff@eurodollaryen.com>
2016-11-28 19:46:52 -0500

    redirect authenticated user to projects index page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  private
+
+  def after_sign_in_path_for(resource)
+   projects_path
+  end
 
   protected
 
