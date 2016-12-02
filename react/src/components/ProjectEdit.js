1f3c35186bbbdf1c3a1eb24853d286469c772df9
jeffreyfriedman <jeff@eurodollaryen.com>
2016-12-01 23:31:59 -0500

    remove redundant code

--- react/src/components/ProjectEdit.js
+++ react/src/components/ProjectEdit.js
@@ -3,17 +3,6 @@ import Autocomplete from './Autocomplete';
 
 const ProjectEdit = props => {
 
-  const eventNormalizer = (selection) => {
-    // create artificial "event" for the App component's handleFieldChange to use
-    let e = {
-      target: {
-        name: "addUser",
-        value: selection.value
-      }
-    }
-    props.handleFieldChange(e);
-  }
-
   return(
     <div>
       {props.colorSelect}
