0d1d9f5578e0720c2eb65cfef6fcf0c2e6d4d0c5
jeffreyfriedman <jeff@eurodollaryen.com>
2016-12-01 19:29:16 -0500

    remove duplicate 'add user' input for projects

--- react/src/components/ProjectEdit.js
+++ react/src/components/ProjectEdit.js
@@ -28,13 +28,12 @@ const ProjectEdit = props => {
     <div>
       {props.colorSelect}
       <input type="text" value={props.editProject} name="editProject" onChange={props.handleFieldChange} />
-      <input type="text" value={props.addUser} name="addUser" onChange={props.handleFieldChange} placeholder="Add User to Project (username)" />
-        <Select.Async
-          name="addUser"
-          loadOptions={getOptions}
-          onChange={eventNormalizer}
-          placeholder="Add User to Project (username)"
-        />
+      <Select.Async
+        name="addUser"
+        loadOptions={getOptions}
+        onChange={eventNormalizer}
+        placeholder="Add User to Project (username)"
+      />
       <div className="buttons project-edit">
       <button className="EditProject btn" onClick={props.handleEdit}>Save Edit</button>
       <button className="Cancel btn" onClick={props.handleCancel}>Cancel</button>
